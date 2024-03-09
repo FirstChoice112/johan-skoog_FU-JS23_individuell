56a93c4d84a9f5e278ca81f00deddd287bc1a6a7
FirstChoice112 <johan.skoog.1986@gmail.com>
2024-03-09 17:13:00 +0100

    store tillagd

---
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,20 @@
+// import "./shoppingCart.scss";
+
+// export default function ShopppingCart() {
+//   return (
+//     <>
+//       <div className="shop__container">
+//         <img
+//           src="src\assets\images\Header\Header_Orders.png"
+//           alt="Bild för antal"
+//         />
+//         <img
+//           className="btn__cart"
+//           src="src\assets\images\Header\Header_background.png"
+//           alt="Bild för antal"
+//         />
+//         <span>0</span>
+//       </div>
+//     </>
+//   );
+// }
---
+++ src/store/Store.jsx
@@ -0,0 +1,12 @@
+import { createStore } from "zustand";
+const useCounterStore = createStore((set) => ({
+  count: 0,
+  // Funktion för att öka räknaren
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  // Funktion för att minska räknaren
+  decrement: () => set((state) => ({ count: state.count - 1 })),
+  // Funktion för att återställa räknaren till noll
+  reset: () => set({ count: 0 }),
+}));
+
+export default useCounterStore;
